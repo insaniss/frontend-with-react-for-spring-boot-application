8ad06d70e1b9de494bd53c3e57df58a74a5c9c88
Bobur Zakirov <bobur.octo@gmail.com>
2020-12-15 15:29:40 +0300

    Login/Register rewrote as functional components

--- src/component/Login.js
+++ src/component/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Input} from 'react-toolbox/lib/input';
 
 import './Login.scss';
@@ -6,135 +6,92 @@ import {isValidUsername} from '../service/validator/user';
 import {isValidPassword} from '../service/validator/user';
 import {login} from '../service/request/user';
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.loginForm = React.createRef();
-    this.loginMessage = React.createRef();
+export const Login = React.forwardRef((props, ref) => {
 
-    this.state = {
-      username: "", usernameOk: null,
-      password: "", passwordOk: null
-    }
+  const [username, setUsername] = useState('');
+  const [usernameOk, setUsernameOk] = useState(null);
+  const [password, setPassword] = useState('');
+  const [passwordOk, setPasswordOk] = useState(null);
+  const [message, setMessage] = useState('');
+
+  const validateValues = () => {
+    setUsernameOk(isValidUsername(username));
+    setPasswordOk(isValidPassword(password));
+    return isValidUsername(username) && isValidPassword(password);
   }
 
-
-  validateValues = () => {
-    this.setState({
-      username: this.state.username, usernameOk: isValidUsername(this.state.username),
-      password: this.state.password, passwordOk: isValidPassword(this.state.password)
-    })
-    return isValidUsername(this.state.username) && isValidPassword(this.state.password);
-  }
-
-
-  handleLoginSubmit = (e) => {
+  const handleLoginSubmit = (e) => {
     e.preventDefault();
-    this.loginMessage.current.innerHTML = '';
 
-    if (this.validateValues()) {
-      login(this.state.username, this.state.password).then(response => {
+    setMessage('');
+    if (validateValues()) {
+      login(username, password).then(response => {
         if (response.status === 200) {
-          response.text().then(token => {localStorage.setItem("auth", token)});
-          this.hideLoginForm();
+          response.text().then(token => {
+            localStorage.setItem("auth", token);
+            hideLoginForm();
+          });
         } else if (response.status === 401) {
-          response.text().then(text => {this.loginMessage.current.innerHTML = text});
+          response.text().then(text => {setMessage(text)});
         } else {
-          this.registerMessage.current.innerHTML = "Something went wrong, please try again";
+          setMessage("Something went wrong, please try again");
         }
       });
     }
   }
 
-
-  handleUsernameChange = (username) => {
-    this.setState({
-      username: username, usernameOk: this.state.usernameOk,
-      password: this.state.password, passwordOk: this.state.passwordOk
-    });
-  }
-  handlePasswordChange = (password) => {
-    this.setState({
-      username: this.state.username, usernameOk: this.state.usernameOk,
-      password: password, passwordOk: this.state.passwordOk
-    });
+  const setAllInitialValue = () => {
+    setUsername('');
+    setUsernameOk(null);
+    setPassword('');
+    setPasswordOk(null);
+    setMessage('');
   }
 
-  setStateNull = () => {
-    this.loginMessage.current.innerHTML = '';
-    this.setState({
-      username: "", usernameOk: null,
-      password: "", passwordOk: null
-    })
+  const showLoginForm = () => {
+    props.setFormVisibility("login", "block");
+  }
+  const hideLoginForm = () => {
+    props.setFormVisibility("login", "none");
+    setAllInitialValue();
+  }
+  const redirectToRegister = () => {
+    props.setFormVisibility("register", "block");
+    hideLoginForm();
   }
 
-  // shows display of login form
-  showLoginForm = () => {
-    if (this.loginForm !== undefined && this.loginForm.current != null) {
-      this.loginForm.current.style.display = "block";
-    }
-  }
-
-  // hides display of login form
-  hideLoginForm = () => {
-    if (this.loginForm !== undefined && this.loginForm.current != null) {
-      this.loginForm.current.style.display = "none";
-      this.setStateNull();
-    }
-  }
-
-  redirect = () => {
-    this.hideLoginForm();
-    this.props.registerForm();
-  }
-
-  // react lifecycle method run after the mounting of component
-  componentDidMount() {
-    this.props.onRef(this);
-  }
-  // react lifecycle method run before the mounting of component
-  componentWillUnmount() {
-    this.props.onRef(undefined);
-  }
-
-  render() {
-    return (
-      <div>
-        <input className="LoginButton" type="button" value="Login" onClick={this.showLoginForm}/>
-        <div className="LoginForm" ref={this.loginForm}>
-          <div className="Form">
-            <input type="button" value="X" onClick={this.hideLoginForm}/>
-            <div className="FormGroup">
-              <span className={'title'}>Login</span>
-            </div>
-            <div className="FormGroup">
-              <label htmlFor="username">Username</label>
-              <Input id="username" type="text" value={this.state.username}
-                     autoComplete="off" onChange={e => this.handleUsernameChange(e)}/>
-              {
-                !this.state.usernameOk && this.state.usernameOk != null &&
-                  <small>Allowed only letters, numbers and underscores</small>
-              }       
-            </div>
-            <div className="FormGroup">
-              <label htmlFor="password">Password</label>
-              <Input id="password" type="password" value={this.state.password}
-                     feedback="false" onChange={e => this.handlePasswordChange(e)}/>
-              {
-                !this.state.passwordOk && this.state.passwordOk != null &&
-                  <small>Password must be at least 4 characters long</small>
-              }
-            </div>
-            <div className="FormGroup">
-              <label id="last" htmlFor="continue" ref={this.loginMessage}/>
-              <input id="continue" type="button" value="Continue" onClick={this.handleLoginSubmit}/>
-            </div>
-            <p>Don't have an account? <span onClick={this.redirect}>Register</span></p>
+  return (
+    <div>
+      <input className="LoginButton" type="button" value="Login" onClick={showLoginForm}/>
+      <div className="LoginForm" ref={ref}>
+        <div className="Form">
+          <input type="button" value="x" onClick={hideLoginForm}/>
+          <div className="FormGroup">
+            <span>Login</span>
           </div>
+          <div className="FormGroup">
+            {
+              !usernameOk && usernameOk != null &&
+              <small>Allowed only letters, numbers and underscores</small>
+            }
+            <Input type="text" placeholder="username" autoComplete="off"
+                   value={username} onChange={username => setUsername(username)}/>
+          </div>
+          <div className="FormGroup">
+            {
+              !passwordOk && passwordOk != null &&
+              <small>Password must be at least 4 characters long</small>
+            }
+            <Input type="password" placeholder="password" feedback="false"
+                   value={password} onChange={password => setPassword(password)}/>
+          </div>
+          <div className="FormGroup">
+            <small>{message}</small>
+            <input type="button" value="Continue" onClick={handleLoginSubmit}/>
+          </div>
+          <p>Don't have an account? <span onClick={redirectToRegister}>Register</span></p>
         </div>
       </div>
-    )
-  }
-}
-
-export default Login;
+    </div>
+  )
+})
--- src/component/Register.js
+++ src/component/Register.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, forwardRef} from 'react';
 import {Input} from 'react-toolbox/lib/input';
 
 import './Register.scss';
@@ -7,154 +7,105 @@ import {isValidPassword} from '../service/validator/user';
 import {areEqual} from '../service/validator/user';
 import {register} from '../service/request/user';
 
-class Register extends React.Component {
-  constructor(props) {
-    super(props);
-    this.registerForm = React.createRef();
-    this.registerMessage = React.createRef();
+export const Register = forwardRef((props, ref) => {
 
-    this.state = {
-      username1: "", username1Ok: null,
-      password1: "", password1Ok: null,
-      password2: "", password2Ok: null
-    }
+  const [username1, setUsername1] = useState('');
+  const [username1Ok, setUsername1Ok] = useState(null);
+  const [password1, setPassword1] = useState('');
+  const [password1Ok, setPassWord1Ok] = useState(null);
+  const [password2, setPassword2] = useState('');
+  const [password2Ok, setPassWord2Ok] = useState(null);
+  const [message, setMessage] = useState('');
+
+  const validateValues = () => {
+    setUsername1Ok(isValidUsername(username1));
+    setPassWord1Ok(isValidPassword(password1));
+    setPassWord2Ok(areEqual(password1, password2));
+    return isValidUsername(username1) && isValidPassword(password1) && areEqual(password1, password2);
   }
 
-  validateValues = () => {
-    this.setState({
-      username1: this.state.username1, username1Ok: isValidUsername(this.state.username1),
-      password1: this.state.password1, password1Ok: isValidPassword(this.state.password1),
-      password2: this.state.password2, password2Ok: areEqual(this.state.password1, this.state.password2)
-    })
-    return isValidUsername(this.state.username1) && isValidPassword(this.state.password1)
-      && areEqual(this.state.password1, this.state.password2);
-  }
-
-
-  handleRegisterSubmit = (e) => {
+  const handleRegisterSubmit = (e) => {
     e.preventDefault();
-    this.registerMessage.current.innerHTML = '';
 
-    if (this.validateValues()) {
-      register(this.state.username1, this.state.password1).then(response => {
+    setMessage('');
+    if (validateValues()) {
+      register(username1, password1).then(response => {
         if (response.status === 200) {
-          response.text().then(token => {localStorage.setItem("auth", token);});
-          this.hideRegisterForm();
+          response.text().then(token => {
+            localStorage.setItem("auth", token);
+            hideRegisterForm();
+          });
         } else if (response.status === 409) {
-          response.text().then(text => {this.registerMessage.current.innerHTML = text;});
+          response.text().then(text => {setMessage(text)});
         } else {
-          this.registerMessage.current.innerHTML = "Something went wrong, please try again";
+          setMessage("Something went wrong, please try again");
         }
       });
     }
   }
 
-  handleUsername1Change = (username1) => {
-    this.setState({
-      username1: username1, username1Ok: this.state.username1Ok,
-      password1: this.state.password1, password1Ok: this.state.password1Ok,
-      password2: this.state.password2, password2Ok: this.state.password2Ok
-    });
-  }
-  handlePassword1Change = (password1) => {
-    this.setState({
-      username1: this.state.username1, username1Ok: this.state.username1Ok,
-      password1: password1, password1Ok: this.state.password1Ok,
-      password2: this.state.password2, password2Ok: this.state.password2Ok
-    });
-  }
-  handlePassword2Change = (password2) => {
-    this.setState({
-      username1: this.state.username1, username1Ok: this.state.username1Ok,
-      password1: this.state.password1, password1Ok: this.state.password1Ok,
-      password2: password2, password2Ok: this.state.password2Ok
-    });
+  const setAllInitialValue = () => {
+    setUsername1('');
+    setUsername1Ok(null);
+    setPassword1('');
+    setPassWord1Ok(null);
+    setPassword2('');
+    setPassWord2Ok(null);
+    setMessage('');
   }
 
-  setStateNull = () => {
-    this.registerMessage.current.innerHTML = '';
-    this.setState({
-      username1: "", username1Ok: null,
-      password1: "", password1Ok: null,
-      password2: "", password2Ok: null
-    })
+  const showRegisterForm = () => {
+    props.setFormVisibility("register", "block");
+  }
+  const hideRegisterForm = () => {
+    props.setFormVisibility("register", "none");
+    setAllInitialValue();
+  }
+  const redirectToLogin = () => {
+    props.setFormVisibility("login", "block");
+    hideRegisterForm();
   }
 
-  // shows display of register form
-  showRegisterForm = () => {
-    if (this.registerForm !== undefined && this.registerForm.current != null) {
-      this.registerForm.current.style.display = "block";
-    }
-  }
-  // hides display of register form
-  hideRegisterForm = () => {
-    if (this.registerForm !== undefined && this.registerForm.current != null) {
-      this.registerForm.current.style.display = "none";
-      this.setStateNull();
-    }
-  }
-
-  redirect = () => {
-    this.hideRegisterForm();
-    this.props.loginForm();
-  }
-
-  // react lifecycle method run after the mounting of component
-  componentDidMount() {
-    this.props.onRef(this);
-  }
-  // react lifecycle method run before the mounting of component
-  componentWillUnmount() {
-    this.props.onRef(undefined);
-  }
-
-  render() {
-    return(
-      <div>
-        <input className="RegisterButton" type="button" value="Register" onClick={this.showRegisterForm}/>
-        <div className="RegisterForm" ref={this.registerForm}>
-          <div className="Form">
-            <input type="button" value="X" onClick={this.hideRegisterForm}/>
-            <div className="FormGroup">
-              <span className={'title'}>Register</span>
-            </div>
-            <div className="FormGroup">
-              <label htmlFor="username1">Username</label>
-              <Input id="username1" type="text" value={this.state.username1}
-                     autoComplete="off" onChange={e => this.handleUsername1Change(e)}/>
-              {
-                !this.state.username1Ok && this.state.username1Ok != null &&
-                  <small>Allowed only letters, numbers and underscores</small>
-              }
-            </div>
-            <div className="FormGroup">
-              <label htmlFor="password1">Password</label>
-              <Input id="password1" type="password" value={this.state.password1}
-                     feedback="false" onChange={e => this.handlePassword1Change(e)}/>
-              {
-                !this.state.password1Ok && this.state.password1Ok != null &&
-                  <small>Password must be at least 4 characters long</small>
-              }
-            </div>
-            <div className="FormGroup">
-              <label htmlFor="password2">Confirm password</label>
-              <Input id="password2" type="password" value={this.state.password2}
-                     feedback="false" onChange={e => this.handlePassword2Change(e)}/>
-              {
-                !this.state.password2Ok && this.state.password2Ok != null &&
-                  <small>Confirmation password mismatched</small>
-              }
-            </div>
-            <div className="FormGroup">
-              <label id="last" htmlFor="continue" ref={this.registerMessage}/>
-              <input id="continue1" type="button" value="Continue" onClick={this.handleRegisterSubmit}/>
-            </div>
-            <p>Already have an account? <span onClick={this.redirect}>Login</span></p>
+  return (
+    <div>
+      <input className="RegisterButton" type="button" value="Register" onClick={showRegisterForm}/>
+      <div className="RegisterForm" ref={ref}>
+        <div className="Form">
+          <input type="button" value="x" onClick={hideRegisterForm}/>
+          <div className="FormGroup">
+            <span>Register</span>
           </div>
+          <div className="FormGroup">
+            {
+              !username1Ok && username1Ok != null &&
+              <small>Allowed only letters, numbers and underscores</small>
+            }
+            <Input type="text" placeholder="username" autoComplete="off"
+                   value={username1} onChange={username => setUsername1(username)}/>
+          </div>
+          <div className="FormGroup">
+            {
+              !password1Ok && password1Ok != null &&
+              <small>Password must be at least 4 characters long</small>
+            }
+            <Input type="password" placeholder="password" feedback="false"
+                   value={password1} onChange={password => setPassword1(password)}/>
+          </div>
+          <div className="FormGroup">
+            {
+              !password2Ok && password2Ok != null &&
+              <small>Confirmation password mismatched</small>
+            }
+            <Input type="password" placeholder="confirm password" feedback="false"
+                   value={password2} onChange={password => setPassword2(password)}/>
+          </div>
+          <div className="FormGroup">
+            <small>{message}</small>
+            <input id="register" type="button" value="Continue" onClick={handleRegisterSubmit}/>
+          </div>
+          <p>Already have an account? <span onClick={redirectToLogin}>Login</span></p>
         </div>
       </div>
-    )
-  }
-}
-
-export default Register;
+    </div>
+  )
+})
